188fae18045fad59dac4fefa1180a966bcc30ede
frdomovic <93442516+frdomovic@users.noreply.github.com>
2022-05-16 16:27:22 +0200

    Added login screen design and logic

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d25036c62e82e375"; }
+/******/ 		__webpack_require__.h = function() { return "37070c6094bedcf7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
